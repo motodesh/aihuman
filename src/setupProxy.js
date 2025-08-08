61dac83312ee5de9542cab65b1a9a332278b737c
motodesh <motodesh@gmail.com>
2025-08-08 14:06:40 +0000

    Added proxy for authentication fix

---
+++ src/setupProxy.js
@@ -0,0 +1,15 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/',
+    createProxyMiddleware({
+      target: 'http://localhost:3000',
+      changeOrigin: true,
+      headers: {
+        'Cross-Origin-Opener-Policy': 'same-origin',
+        'Cross-Origin-Embedder-Policy': 'require-corp',
+      },
+    })
+  );
+};
