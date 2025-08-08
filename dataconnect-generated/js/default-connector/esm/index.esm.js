9172aa29d23ac24fe6114169d9764d011a7160ed
motodesh <motodesh@gmail.com>
2025-08-08 13:45:40 +0000

    deployed in firebase of google motodesh

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'aihuman',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'aihuman',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
